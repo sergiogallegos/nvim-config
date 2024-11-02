0aa1137efa3a08344c8aea184fcbb5ecb2734917
sergiogallegos <gallegoshinojosa@gmail.com>
2024-11-02 15:51:05 -0600

    nvim gruvbox hard

--- lua/sergio/plugins/colorscheme.lua
+++ lua/sergio/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
   priority = 1000,
   config = function()
     -- Set up Gruvbox Material with custom options
-    vim.g.gruvbox_material_background = "soft" -- Options: 'hard', 'medium', 'soft'
+    vim.g.gruvbox_material_background = "hard" -- Options: 'hard', 'medium', 'soft'
     vim.g.gruvbox_material_enable_italic = 1
     vim.g.gruvbox_material_transparent_background = 1 -- Enable transparency
     vim.g.gruvbox_material_disable_italic_comment = 1
