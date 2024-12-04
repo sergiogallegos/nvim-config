3f2ed56f5dc3354b3f7a07894d6bc1ac31ed7cf7
Sergio Gallegos <gallegoshinojosa@gmail.com>
2024-12-03 19:55:49 -0700

    sg

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -1,20 +1,11 @@
 return {
-  {
-    "tjdevries/colorbuddy.nvim",    -- Load colorbuddy.nvim first
-    lazy = false,
-  },
-  {
-    "tjdevries/gruvbuddy.nvim",      -- Load gruvbuddy.nvim with colorbuddy as a dependency
-    dependencies = { "tjdevries/colorbuddy.nvim" },
-    config = function()
-      require("colorbuddy").setup()  -- Ensure colorbuddy is initialized
-      vim.cmd.colorscheme("gruvbuddy") -- Set the color scheme to gruvbuddy
-    end,
-    lazy = false,
-    priority = 1000,
-  },
-  -- Other colorschemes as fallbacks or options
-  "rktjmp/lush.nvim",
-  { "rose-pine/neovim", name = "rose-pine" },
-  -- Add more colorschemes if needed
-}
+    {
+      "sergiogallegos/colorbuddy.nvim",
+      config = function()
+        require("colorbuddy").setup()  -- Ensure colorbuddy is initialized
+        vim.cmd.colorscheme("gruvbuddy") -- Set the color scheme to gruvbuddy
+      end,
+      lazy = false,
+      priority = 1000,
+    },
+  }
