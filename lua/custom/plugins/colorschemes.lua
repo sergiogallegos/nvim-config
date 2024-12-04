96e3491450ec0ed42a7a30241494bc502ea03890
Sergio Gallegos <gallegoshinojosa@gmail.com>
2024-12-03 23:28:10 -0700

    custombuddy statusline

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -3,7 +3,7 @@ return {
       "sergiogallegos/colorbuddy.nvim",
       config = function()
         require("colorbuddy").setup()  -- Ensure colorbuddy is initialized
-        vim.cmd.colorscheme("gruvbuddy") -- Set the color scheme to gruvbuddy
+        vim.cmd.colorscheme("custombuddy") -- Set the color scheme to gruvbuddy
       end,
       lazy = false,
       priority = 1000,
