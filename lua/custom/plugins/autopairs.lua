85b75f1418e1f94588f8c3c2974a14ac4828d7d9
sergiogallegos <gallegoshinojosa@gmail.com>
2025-10-04 23:41:14 -0600

    feat: disable automatic bracket completion
    - Disable nvim-autopairs plugin completely
    - Remove automatic bracket/parenthesis/quote completion
    - Allow manual typing of all brackets and parentheses
    - Provide full control over bracket placement
    
    Changes:
    - lua/custom/plugins/autopairs.lua: Set enabled = false to disable autopairs

--- lua/custom/plugins/autopairs.lua
+++ lua/custom/plugins/autopairs.lua
@@ -2,33 +2,10 @@ return {
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
+    enabled = false,  -- Disable autopairs completely
     config = function()
-      local autopairs = require("nvim-autopairs")
-      autopairs.setup({
-        check_ts = true,
-        ts_config = {
-          lua = { "string", "source" },
-          javascript = { "string", "template_string" },
-          java = false,
-        },
-        disable_filetype = { "TelescopePrompt", "spectre_panel" },
-        fast_wrap = {
-          map = "<M-e>",
-          chars = { "{", "[", "(", '"', "'" },
-          pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-          offset = 0,
-          end_key = "$",
-          keys = "qwertyuiopzxcvbnmasdfghjkl",
-          check_comma = true,
-          highlight = "PmenuSel",
-          highlight_grey = "LineNr",
-        },
-      })
-
-      -- Integration with nvim-cmp
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      local cmp = require("cmp")
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+      -- Autopairs is disabled - no automatic bracket completion
+      -- You can manually type all brackets, parentheses, quotes, etc.
     end,
   },
 }
